16f150c54f4e486beac6e7757fb174021c3f55ac
eloy <eloydominguez3@proton.me>
2025-05-23 10:41:45 +0200

    Ejercicio_Ventas_09

--- backend/controller/ventasController.js
+++ backend/controller/ventasController.js
@@ -10,3 +10,15 @@ exports.verventas = async (req,res) => {
 
     }
 };
+
+exports.crearventas = async (req,res) => {
+
+    const { fecha  } = req.body;
+    
+    try{ await db.query('INSERT INTO ventas (fecha) VALUES (?)',[fecha]);
+        res.json({ message: 'Producto creado correctamente' });
+    } catch (error) {
+       console.log('Error al crear producto:', error);
+       res.status(500).json({ error: 'Error al crear producto' });
+    }
+};
