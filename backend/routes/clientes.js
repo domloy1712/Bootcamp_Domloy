0cf36030efc27204ab23d3d107dc4b91486705c1
eloy <eloydominguez3@proton.me>
2025-05-23 12:28:41 +0200

    Ejercicio_Ventas_11

--- backend/routes/clientes.js
+++ backend/routes/clientes.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const Router = express.Router();
+const clientesController = require('../controller/clientesController');
 
+router.get('/', clientesController.mostrar);
+router.post('/', clientesController.crear);
+router.delete('/:id', )
 
 
 
