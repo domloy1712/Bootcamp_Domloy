3913f419bcd944fff896a76ea406df64a472ba34
eloy <eloydominguez3@proton.me>
2025-05-26 11:29:12 +0200

    Eliminar

--- backend/routes/productos.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const prcontroller = require('../controller/productosController');
-
-router.get('/', prcontroller.productos);
-router.get('/:id', prcontroller.verproducto);
-router.post('/', prcontroller.crearproductos);
-router.delete('/:id', prcontroller.eliminar);
-module.exports= router;
