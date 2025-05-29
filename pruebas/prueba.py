22075ce6be64f23e2e2d71f65e0bdddfd1ce1445
eloy <eloydominguez3@proton.me>
2025-05-29 12:35:29 +0200

    Pyton

--- pruebas/prueba.py
+++ pruebas/prueba.py
@@ -1,20 +1,16 @@
 Usuario = input("Introduzca su usuario")
 Año_de_Nacimiento = int(input("Introduzca su año de nacimiento"))
-print("Tu usuario", Usuario)
-print( "Tu año de nacimiento es:", Año_de_Nacimiento)
-print("Años:", 2025 - Año_de_Nacimiento)
 Residencia = input("Donde vive?")
-print("vive en:", Residencia)
 mascota = input("Tienes Mascota? si/no")
 Edad = 2025 - Año_de_Nacimiento
+Color = input("Dame un color")
+
 
 if (mascota == "si"):
     print ("Muy bien")
 else:
     print("Muy mal estaras solo en la vida pringado")
 
-Color = input("Dame un color")
-print("su color", Color)
 
 if ( Edad < 18 ):
     print ("Es menor de edad")
@@ -25,4 +21,8 @@ elif ( Edad > 60 ):
 
 
 
-
+print("Tu usuario", Usuario)
+print( "Tu año de nacimiento es:", Año_de_Nacimiento)
+print("Años:", Edad)
+print("vive en:", Residencia)
+print("su color", Color)
