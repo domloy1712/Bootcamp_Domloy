762401172d394c0911d0e4664f75fb53f8f8ccad
eloy <eloydominguez3@proton.me>
2025-05-29 09:24:11 +0200

    Eliminado la Rama

--- backend/routes/ventas.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const Router = express.Router();
-const ventascontroller = require('../controller/ventasController')
-
-Router.get('/', ventascontroller.verventas)
-Router.post('/', ventascontroller.crearventas)
-
-module.exports = Router;
