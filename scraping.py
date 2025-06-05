d934a932c321f20550f9a9f13a9cec2cda4be63f
eloy <eloydominguez3@proton.me>
2025-06-05 13:16:27 +0200

    Pyton_18

---
+++ scraping.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+import csv 
+import schedule
+import time
+
+info = []
+url = 'https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops'
+
+def pasarlo_a_csv(filename,info):
+    with open( filename, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Precio'])  # Escribir encabezados
+        writer.writerows(info)
+
+def cada_cierto_tiempo(url):
+    schedule.every(60).seconds.do(lambda: scrapear(url))
+    while True:
+        schedule.run_pending()
+        
+
+def scrapear(url):
+   responder = requests.get(url)
+   if responder.status_code == 200:
+        soup = BeautifulSoup(responder.text, 'html.parser')
+        precio = soup.find('span', itemprop='price').text
+        print(f'Precio actualizado: {precio}')
+        pasarlo_a_csv('precios.csv', [[precio]])
+   else:
+        print(f'Error al acceder a la página: {responder.status_code}')
+
+url = 'https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops'
+pasarlo_a_csv('Precios.csv', info)
+cada_cierto_tiempo(url)
